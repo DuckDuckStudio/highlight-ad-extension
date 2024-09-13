dc7c8e870d33312754852b72e528ae2403869740
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-09-13 11:25:06 +0800

    1

--- EXTENSION/content.js
+++ EXTENSION/content.js
@@ -1,18 +1,18 @@
 function highlightAds() {
   const searchWord = '广告';
-  
+
   function highlightText(node) {
     const text = node.textContent;
     const regExp = new RegExp(`(${searchWord})`, 'gi');
     const newHTML = text.replace(regExp, '<span class="highlight">\$1</span>');
-    
+
     if (newHTML !== text) {
       const newNode = document.createElement('span');
       newNode.innerHTML = newHTML;
       node.parentNode.replaceChild(newNode, node);
     }
   }
-  
+
   function traverseDOM(node) {
     if (node.nodeType === Node.TEXT_NODE) {
       highlightText(node);
@@ -22,7 +22,7 @@ function highlightAds() {
       }
     }
   }
-  
+
   traverseDOM(document.body);
 }
 
